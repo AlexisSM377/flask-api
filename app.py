958caf68e16c7504416710095ff46ee1a0e06d15
AlexisSM377 <al222111377@gmail.com>
2024-10-04 12:31:09 -0600

    mvc utilizando sqlachemy

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask 
+from config import Config
+from models import db
 from routes import init_routes
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# Inicializar la base de datos
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 init_routes(app)
 
-
-@app.route('/')
-def home():
-    return {
-        "message" : "Bienvendo api flask Idgs 101"
-    }
 
 
 if __name__ == '__main__':
--- routes/usuarios.py
+++ routes/usuarios.py
@@ -7,10 +7,15 @@ usuarios_routes = Blueprint('usuarios', __name__)
 @usuarios_routes.route('/api/usuarios', methods=['GET'])
 def get_usuarios():
     usuarios = obtener_usuarios()
-    return jsonify(usuarios)
+    usuarios_serializados = [{
+        "id": u.id, 
+        "nombre": u.nombre, 
+        "email": u.email
+    } for u in usuarios]
+    return jsonify(usuarios_serializados)
 
 @usuarios_routes.route('/api/usuarios', methods=['POST'])
-def create_usuario():
+def post_usuario():
     data = request.json
     mensaje = agregar_usuario(data['nombre'], data['email'])
     return jsonify({"mensaje": mensaje})
